1f911ef7742a971207f85be613c65a730b68e4d2
sanihamdhani <sani060200@gmail.com>
2021-02-15 12:43:32 +0700

    alert dialog

--- config/proseslogin.php
+++ config/proseslogin.php
@@ -7,12 +7,10 @@
    $q->execute(array($username));
    $hasil = $q->fetch();
    if($q->rowCount() == 0) {
-     echo "<div align='center'>Username Belum Terdaftar! 
-     <a href='../login.php'>Back</a></div>";
+    echo "<script>alert('Username Belum Terdaftar!');history.go(-1);</script>";
    } else {
      if($password <> $hasil['password']) {
-      echo "<div align='center'>Password Yang Anda Masukan Salah! 
-      <a href='../login.php'>Back</a></div>";
+      echo "<script>alert('Password Salah!');history.go(-1);</script>";
      } else {
        $_SESSION['username'] = $hasil['username'];
        header('location:../menuu.php');
